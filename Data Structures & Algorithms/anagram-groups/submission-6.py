230e6b4c10cb2976c5a440bd73f6a5ec39145dff
S-D123 <161137074+S-D123@users.noreply.github.com>
2026-09-02 15:17:42 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        sortedStr = []
+        for i in range(len(strs)):
+            word = "".join(sorted(strs[i]))
+            sortedStr.append(word)
+
+        d = defaultdict(list)
+        for i in range(len(sortedStr)):
+            word = sortedStr[i]
+            d[word].append(strs[i])
+
+        return [arr for key, arr in d.items()]
